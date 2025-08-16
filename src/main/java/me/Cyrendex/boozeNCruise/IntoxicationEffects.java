a5069d808abe5c400a57384ef223255967228191
Eren Unsal <aaronunsal@gmail.com>
2025-08-16 04:26:03 -0700

    Starting to implement BAC into intoxication meaningfully

---
+++ src/main/java/me/Cyrendex/boozeNCruise/IntoxicationEffects.java
@@ -0,0 +1,15 @@
+package me.Cyrendex.boozeNCruise;
+
+public class IntoxicationEffects {
+
+    public enum Stage {
+        SOBER,
+        S1,
+        S2,
+        S3,
+        S4,
+        S5
+    }
+
+    // TODO: Map online players to each stage depending on the BAC, set thresholds for each stage.
+}
