45e3a092a240fd529b8ff05092f83d85ffa36ff4
Eren Unsal <aaronunsal@gmail.com>
2025-08-16 04:30:20 -0700

    Added temporary thresholds and a boolean flag for fatal BAC

--- src/main/java/me/Cyrendex/boozeNCruise/IntoxicationEffects.java
+++ src/main/java/me/Cyrendex/boozeNCruise/IntoxicationEffects.java
@@ -1,5 +1,9 @@
 package me.Cyrendex.boozeNCruise;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class IntoxicationEffects {
 
     public enum Stage {
@@ -11,5 +15,16 @@ public class IntoxicationEffects {
         S5
     }
 
-    // TODO: Map online players to each stage depending on the BAC, set thresholds for each stage.
+    // Base thresholds for BAC (tolerance value will shift them right)
+    private static final double[] BASES = {0.00, 0.02, 0.06, 0.12, 0.20, 0.30};
+
+    // Instant death over fatal BAC (default will be false)
+    private static final boolean KILL_OVER_45 = false;
+
+    private final AlcoholManager mgr;
+    private final Map<UUID, Stage> lastStage = new HashMap<>();
+
+    public IntoxicationEffects(AlcoholManager mgr) {
+        this.mgr = mgr;
+    }
 }
